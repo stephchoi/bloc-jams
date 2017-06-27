149943753f78220c2414f0af576b7a37c6a328f9
Stephany Choi <s.choi417@gmail.com>
2017-06-27 10:51:19 -0400

    Get scrolling distance from window top

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,20 +1,21 @@
+var pointsArray= document.getElementsByClassName('point');
+
 var animatePoints = function() {
-
-  var points= document.getElementsByClassName('point');
-
-  var revealPoint = function () {
-    for (var i=0; i<points.length; i++){
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform= "scaleX(1) translateY(0)";
-    }
+  var revealPoint = function (index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform= "scaleX(1) translateY(0)";
   };
-   revealPoint();
+
+  for (var i=0; i<points.length; i++){
+      revealPoint(i);
+  }
 };
 
 window.onload = function() {
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
   window.addEventListener('scroll', function(event) {
-    console.log(event);
+    console.log("Current offset from the top is" + sellingPoints.getBoundingClientRect().top + "pixels");
   });
 }
