54b58a30ffcdaf0ae905a916c254c7fb4809a8e3
Stephany Choi <s.choi417@gmail.com>
2017-06-27 10:31:50 -0400

    Assignment 8 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,7 +2,18 @@ var animatePoints = function() {
 
   var points= document.getElementsByClassName('point');
 
-  var revealFirstPoint = function () {
+  var revealPoint = function () {
+    for (var i=0; i<points.length; i++){
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform= "scaleX(1) translateY(0)";
+    }
+  };
+   revealPoint();
+
+
+  /*var revealFirstPoint = function () {
     points[0].style.opacity = 1;
     points[0].style.transform = "scaleX(1) translateY(0)";
     points[0].style.msTransform = "scaleX(1) translateY(0)";
@@ -24,5 +35,5 @@ var animatePoints = function() {
 
   revealFirstPoint();
   revealSecondPoint();
-  revealThirdPoint();
+  revealThirdPoint();*/
 };
