a3dc0682605dce03387ec7a03f07fb70dc435fc2
Stephany Choi <s.choi417@gmail.com>
2017-06-27 10:58:19 -0400

    Animate selling points

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
 var pointsArray= document.getElementsByClassName('point');
 
-var animatePoints = function() {
+var animatePoints = function(points) {
   var revealPoint = function (index) {
     points[index].style.opacity = 1;
     points[index].style.transform = "scaleX(1) translateY(0)";
@@ -15,7 +15,11 @@ var animatePoints = function() {
 
 window.onload = function() {
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
   window.addEventListener('scroll', function(event) {
-    console.log("Current offset from the top is" + sellingPoints.getBoundingClientRect().top + "pixels");
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+      animatePoints(pointsArray);
+    }
   });
 }
