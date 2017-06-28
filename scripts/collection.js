94f7121c07d3b1a54a9275ad2f7cd8480f74275c
Stephany Choi <s.choi417@gmail.com>
2017-06-28 09:23:45 -0400

    checkpoint 10 complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -13,3 +13,11 @@ var collectionItemTemplate=
   + '  </div>'
   + '</div>'
   ;
+
+  window.onload = function() {
+    var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    collectionContainer.innerHTML = '';
+    for (var i = 0; i < 12; i++) {
+      collectionContainer.innerHTML += collectionItemTemplate;
+    }
+  }
