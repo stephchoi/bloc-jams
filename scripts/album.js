f69cbc2c2541dd5fdcaa6d33f4aea31491dbe9f4
Stephany Choi <s.choi417@gmail.com>
2017-06-28 22:40:45 -0400

    Checkpoint 12 finished

--- scripts/album.js
+++ scripts/album.js
@@ -31,7 +31,7 @@ var albumMarconi = {
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
       '<tr class="album-view-song-item">'
-    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + '</td>'
+    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
     + '  <td class="song-item-title">' + songName + '</td>'
     + '  <td class="song-item-duration">' + songLength + '</td>'
     + '</tr>'
@@ -58,7 +58,8 @@ var setCurrentAlbum = function(album) {
   }
 };
 
-var songListContainer =document.getElementsByClassName('album-view-song-list')[0];
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function() {
@@ -69,4 +70,10 @@ window.onload = function() {
       event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
     };
   });
+
+  for (var i=0; i < songRows.length; i++) {
+    songRows[i].addEventListener('mouseleave', function(event) {
+      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    });
+  }
 }
