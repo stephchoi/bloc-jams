b61d32e12b8b77ae8ded87de90b065cbaea404c9
Stephany Choi <s.choi417@gmail.com>
2017-07-05 11:35:34 -0400

    Finish checkpoint 18

--- scripts/album.js
+++ scripts/album.js
@@ -100,64 +100,10 @@ var setCurrentAlbum = function(album) {
   }
 };
 
-var findParentByClassName = function(element, targetClass) {
-  if (element) {
-    var currentParent = element.parentElement;
-    while (currentParent.className !== targetClass && currentParent.className !== null) {
-      currentParent = currentParent.parentElement;
-    }
-    return currentParent;
-  }
-};
-
-var getSongItem = function(element) {
-  switch(element.className) {
-    case 'album-song-button':
-    case 'ion-play':
-    case 'ion-pause':
-        return findParentByClassName(element, 'song-item-number');
-    case 'album-view-song-item':
-    case 'album-view-song-list':
-        return element.querySelector('.song-item-number');
-    case 'song-item-title':
-    case 'song-item-duration':
-        return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-    case 'song-item-number':
-        return element;
-    default:
-        return;
-  }
-};
-
-var clickHandler = function(targetElement) {
-  var songItem = getSongItem(targetElement);
-
-  if (currentlyPlayingSong === null) {
-    songItem.innerHTML = pauseButtonTemplate;
-    currentlyPlayingSong = songItem.getAttribute('data-song-number');
-  } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-    songItem.innerHTML = playButtonTemplate;
-    currentlyPlayingSong = null;
-  } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-    var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-    currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-    songItem.innerHTML = pauseButtonTemplate;
-    currentlyPlayingSong = songItem.getAttribute('data-song-number');
-  }
-};
-
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var currentlyPlayingSong = null;
 
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);
-
-  for (var i=0; i < songRows.length; i++) {
-    songRows[i].addEventListener('click', function(event){
-      clickHandler(event.target);
-    });
-  }
 });
